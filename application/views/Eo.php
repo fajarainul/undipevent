74906acdf134cdef2669ec8ab1be37f24c5e4bf7
Febra <m.febras@yahoo.com>
2015-06-05 09:49:26 +0700

    Perbaikan halaman daftar EO dan Sponsor

--- application/views/Eo.php
+++ application/views/Eo.php
@@ -1,26 +1,30 @@
-<div id="main_content" style="min-height: 500px">
+<div id="eo_list" class="page_list" style="min-height: 500px">
     <div class="row">
         <h3></h3>
         <div class="col-md-4">
-            <ul class="list_eo">
-                <li><a>Universitas Diponegoro</a></li>
-                <li><a>Fakultas Teknik</a></li>
-                <li><a>fakultas Hukum</a></li>
+            <ul class="nav">
+                <li><a href="#">Universitas Diponegoro</a></li>
+                <li><a href="#">Fakultas Teknik</a></li>
+                <li><a href="#">fakultas Hukum</a></li>
             </ul>
         </div>
         <div class="col-md-4">
-            <ul class="list_eo">
-                <li><a>Universitas Diponegoro</a></li>
-                <li><a>Fakultas Teknik</a></li>
-                <li><a>fakultas Hukum</a></li>
+            <ul class="nav">
+                <li><a href="#">Universitas Diponegoro</a></li>
+                <li><a href="#">Fakultas Teknik</a></li>
+                <li><a href="#">fakultas Hukum</a></li>
             </ul>
         </div>
         <div class="col-md-4">
-           <ul class="list_eo">
-                <li><a>Universitas Diponegoro</a></li>
+           <ul class="nav">
+                <li><a href="#">Universitas Diponegoro</a></li>
                 <li><a>Fakultas Teknik</a></li>
                 <li><a>fakultas Hukum</a></li>
             </ul>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $('#navbar a:eq(1)').attr('class', 'active').css("color", "#ff6600");
+</script>
--- application/views/Sponsor.php
+++ application/views/Sponsor.php
@@ -1,22 +1,22 @@
-<div id="main_content" style="min-height: 500px">
+<div id="sponsor_list" class="page_list" style="min-height: 500px">
     <div class="row">
         <h3></h3>
         <div class="col-md-4">
-            <ul class="list_eo">
+            <ul class="nav">
                 <li><a>Google</a></li>
                 <li><a>Microsoft</a></li>
                 <li><a>Oracle</a></li>
             </ul>
         </div>
         <div class="col-md-4">
-            <ul class="list_eo">
+            <ul class="nav">
                 <li><a>PT. Dirgatara Indonesia</a></li>
                 <li><a>Facebook</a></li>
                 <li><a>Twetter</a></li>
             </ul>
         </div>
         <div class="col-md-4">
-           <ul class="list_eo">
+           <ul class="nav">
                 <li><a>Duren Works</a></li>
                 <li><a>PT. JTI</a></li>
                 <li><a>Indosat</a></li>
@@ -24,3 +24,7 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $('#navbar a:eq(2)').attr('class', 'active').css("color", "#ff6600");
+</script>

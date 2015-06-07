89aa99a11da8b40bb557fd938220b5acfcb35c6e
Febra <m.febras@yahoo.com>
2015-06-07 13:44:06 +0700

    Perbaikan detail event : recent updates, event atribute, gambar

--- application/views/Detail.php
+++ application/views/Detail.php
@@ -5,24 +5,22 @@
                     <div class="event-name">
                         <h2>Nama Event</h2>
                     </div>
-                    <div class="row">
-                        <hr>
-                        <div id="event-attributes">
-                            <div class="col-md-4">
-                                <span class="attribute-icon"><img src="assets/images/eo.png" /></span> <span>Event Organizer</span>
+                    <div id="event-attributes">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <span class="attribute-icon"><img src="<?php echo base_url()?>assets/images/eo.png" /></span> <span>Event Organizer</span>
                             </div>
-                            <div class="col-md-4">
-                                <span class="attribute-icon"><img src="assets/images/calendar.png" /></span> <span>Tanggal Event</span>
+                            <div class="col-sm-3">
+                                <span class="attribute-icon"><img src="<?php echo base_url()?>assets/images/calendar.png" /></span> <span>Tanggal Event</span>
                             </div>
-                            <div class="col-md-4">
-                                <span class="attribute-icon"><img src="assets/images/location.png" /></span> <span>Location</span>
+                            <div class="col-sm-6">
+                                <span class="attribute-icon"><img src="<?php echo base_url()?>assets/images/location.png" /></span> <span>Jl. Prof. Soedarto Gedung E lantai 3 FSM</span>
                             </div>
                         </div>
-                        <hr>
                     </div>
                     <div id="event-content">
                         <div id="event-image">
-                            <img src="assets/images/" />
+                            <div class="thumbnail"><img src="<?php echo base_url()?>assets/images/" /></div>
                         </div>
                         <div id="event-description">
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
@@ -33,18 +31,18 @@
                     </div>
                     <!-- Events terkait-->
                     <div id="other-events">
-                        <div class="col-md-4">
-                            <img src="assets/images">
+                        <div class="col-sm-4">
+                            <div class="thumbnail"><img src="<?php echo base_url()?>assets/images"></div>
                             <h4> Nama Event </h4>
                             <p>Tanggal Event</p>
                         </div>
-                        <div class="col-md-4">
-                            <img src="assets/images">
+                        <div class="col-sm-4">
+                            <div class="thumbnail"><img src="<?php echo base_url()?>assets/images"></div>
                             <h4> Nama Event </h4>
                             <p>Tanggal Event</p>
                         </div>
-                        <div class="col-md-4">
-                            <img src="assets/images">
+                        <div class="col-sm-4">
+                            <div class="thumbnail"><img src="<?php echo base_url()?>assets/images"></div>
                             <h4> Nama Event </h4>
                             <p>Tanggal Event</p>
                         </div>
@@ -53,32 +51,24 @@
 
                 <!-- Kanan -->
                 <div class="col-md-3">
-                    <div id="recent-events">
-                        <div id="sidebar-title">
-                            <h3>Recent Updates</h3>
-                        </div>
-                        <div id="sidebar-content">
-                            <div class="row">
-                                <h5>Nama Event</h5>
-                                <span>Event Organizer</span>
-                                <span>Tanggal Event</span>
-                            </div>
-                            <div class="row">
-                                <h5>Nama Event</h5>
-                                <span>Event Organizer</span>
-                                <span>Tanggal Event</span>
-                            </div>
-                            <div class="row">
-                                <h5>Nama Event</h5>
-                                <span>Event Organizer</span>
-                                <span>Tanggal Event</span>
-                            </div>
-                            <div class="row">
-                                <h5>Nama Event</h5>
-                                <span>Event Organizer</span>
-                                <span>Tanggal Event</span>
-                            </div>
-                        </div>
+                    <div id="recent_updates">
+                        <div class="head_recent_updates">Recent Updates</div>
+                        <?php
+                        $i = 1;
+                        for ($i == 1; $i <= 6; $i++) {
+                            echo"<div class='item_recent_update'>
+                                    <div class='item_recent_update_title'>
+                                        <Strong>Nama Event " . $i . "</strong>
+                                    </div>
+                                    <div class='item_recent_update_note'>
+                                        <small>Himpunan Mahasiswa Teknik Informatika </small>
+                                    </div>
+                                    <div class='item_recent_update_note'>
+                                        <small>1 September 2015</small>
+                                    </div>
+                                </div>";
+                        }
+                        ?>
                     </div>
                 </div> <!-- End Kanan -->
             </div> <!-- End Container -->
